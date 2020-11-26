50a1ffb96e3b80a93bdb7845849061bff7c4aee8
SKYC <13677727680@163.com>
2020-11-26 21:10:10 +0800

    Update Startup.cs

--- Neko.MVC/Neko.MVC/Startup.cs
+++ Neko.MVC/Neko.MVC/Startup.cs
@@ -32,10 +32,10 @@ namespace Neko.MVC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddDbContextPool<NekoDbContext>(op =>
             {
-                op.UseMySQL(_configuration.GetConnectionString("Mysql"));
+                op.UseMySQL(_configuration.GetConnectionString("Mysql_Preview"));
                 op.UseLazyLoadingProxies();
             });
             services.AddSession();
